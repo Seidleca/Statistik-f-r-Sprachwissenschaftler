eeb77e7d7fb7ebccb01a6610573d228a4de7e017
Seidlercarmen <Seidlerc@students.uni-marburg.de>
2014-06-22 21:25:10 +0200

    Updade Fehler

--- Seidlerc/hausaufgabe18.R
+++ Seidlerc/hausaufgabe18.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 18
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-06-17
+# Carmen Seidler <seidlerc@students.uni-marburg.de>
+# 2014-06-22
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 
@@ -45,17 +45,18 @@ y = x1 + x2
 linreg <- data.frame(x1,x2,y)
 
 # Wir können y ~ x1 und y ~ x2 einzel plotten:
-# ggplot(linreg,aes(x=x1,y=y)) + geom_point() + geom_smooth(method="lm")
-# ggplot(linreg,aes(x=x2,y=y)) + geom_point() + geom_smooth(method="lm")
+ ggplot(linreg,aes(x=x1,y=y)) + geom_point() + geom_smooth(method="lm")
+ ggplot(linreg,aes(x=x2,y=y)) + geom_point() + geom_smooth(method="lm")
 
 # Die Linie passt sehr gut zu den Punkten, was wir hätten erwarten sollen, denn
 # wir haben y aus einfachen Summen von x1 und x2 berechnet. Wir berechnen
 # zunächst die lineare Regression für die einzelnen unabhängige Variablen.
 
-# CODE_HIER (x1)
+linreg.lm1 <- lm(y ~ x1, data=linreg)
+summary(linreg.lm1)
 
-
-# CODE_HIER (x2)
+linreg.lm2 <- lm(y ~ x2, data=linreg)
+summary(linreg.lm2)
 
 # Was haben Sie für Koeffizeinten bekommen? Wenn wir daran denken, dass x2 = 2*x1 ist, wissen wir, dass 
 # y = x1 + x2
@@ -84,7 +85,9 @@ print(model.summary)
 # passiert, wenn wir die Reihenfolge von x1 und x2 in lm() umstellen? Führen Sie
 # die passende Regression aus:
 
-# CODE_HIER
+model2 <- lm(y ~ x2 + x1, data=linreg)
+model2.summary <- summary(model)
+print(model2.summary)
 
 # Bei linearen Regression müssen wir immer aufpassen, dass unsere Prediktoren
 # nicht zu stark miteinander korrelieren. Das könnten wir auch mit cor()
@@ -99,43 +102,58 @@ pyreg <- read.table("Data/pyreg.tab",header=TRUE)
 # Wie linreg hat pyreg drei Spalten x1, x2, y
 # Plotten Sie die Punkte + Regressionslinie für y ~ x1 (wie oben).
 
-# CODE_HIER
+ggplot(pyreg,aes(x=x1,y=y)) + geom_point() + geom_smooth(method="lm")
 
 # Und das gleiche für y ~ x2. 
 
-# CODE_HIER
+ggplot(pyreg,aes(x=x2,y=y)) + geom_point() + geom_smooth(method="lm")
 
 # Berechnen Sie die zwei Regressionsmodelle für y ~ x1 und y ~ x2
 
-# CODE_HIER
+pyreg.lm1 <- lm(y ~ x1, data=pyreg)
+summary(pyreg.lm1)
 
-# CODE_HIER
+pyreg.lm2 <- lm(y ~ x2, data=pyreg)
+summary(pyreg.lm2)
 
 # Bevor Sie die Regression y ~ x1 + x2 berechnen, schauen Sie sich die
 # Korrelation (mit Konfidenzintervall!) zwischen x1 und x2 an:
 
-# CODE_HIER
+pyreg.lm3 <- lm(x1 ~ x2, data=pyreg)
+summary(pyreg.lm3)
 
 # Wenn Sie nicht miteinander signifikant korreliert sind, sollten Sie auch die
 # Regression y ~ x1 + x2 berechnen:
 
-# CODE_HIER
+cor.test(pyreg$x1, pyreg$x2, method="pearson")
 
 # Wie gut passt das lineare Modell zu den Daten? Schauen Sie sich die R^2 und 
 # F-Werte an sowie auch die t-Werte für die einzelnen Prediktoren. Glauben Sie, 
 # dass y im linearen Verhältnis zu x1 und x2 steht? Machen Sie eine Grafik wie
 # oben für y ~ x1 + x2, **nachdem Sie sich eine Antwort überlegt haben**.
 
-# CODE_HIER
+ggplot(pyreg,aes(x=x1,y=x2)) + geom_point(aes(size=y))
+
+# Lineares Modell passt nicht sehr gut, da die Punkte sozusagen "kreuz und quer"
+# verteilt sind.
+
 
 # Glauben Sie jetzt, dass y im linearen Verhältnis zu x1 und x2 steht? Warum (nicht)?
 
 # Wie sieht mit Korrelationen aus? Berechnen Sie die Korrelation (sowohl Pearson
 # als auch Spearman) zwischen (y und x1) sowie auch zwischen (y und x2). 
 
-# CODE_HIER
+cor1.pearson <- cor.test(pyreg$y, pyreg$x1, method="pearson")
+print(cor1.pearson)
 
-# CODE_HIER 
+cor1.spearman <- cor.test(pyreg$y, pyreg$x1, method="spearman")
+print(cor1.spearman)
+
+cor2.pearson <- cor.test(pyreg$y, pyreg$x2, method="pearson")
+print(cor2.pearson)
+
+cor2.spearman <- cor.test(pyreg$y, pyreg$x2, method="spearman")
+print(cor2.spearman)
 
 # Welche Art von Korrelation macht am meisten Sinn bei diesen Daten?
 
